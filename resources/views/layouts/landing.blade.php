6b0e2c095d576a661929b67ad16492c0dca147c9
IkhwanWahyudi <ikhwanw06@gmail.com>
2023-10-05 18:16:25 +0800

    Update Repository

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="icon" type="images/png" href="{{ asset('assets/images/logo-removebg-preview.png') }}">
+    <link rel="icon" type="images/png" href="{{ asset('assets/images/logo-black.png') }}">
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap"
         rel="stylesheet">
     <title>Shoes.co</title>
